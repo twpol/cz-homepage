49a5eca11f28ca928d3107ae6a00ac63212dcafe
silver%warwickcompsoc.co.uk <silver%warwickcompsoc.co.uk>
2004-03-10 05:04:26 +0000

    Updating 0.9.61 fix list.

--- revs.js
+++ revs.js
@@ -1,10 +1,14 @@
 var revs =
 [
   {id: "0.9.61", date: "03/10/2004", condition: G,
-   bugs: [229072, 235260, 235261, 210222, 235648, 226408],
+   bugs: [229072, 235260, 235261, 210222, 235648, 226408, 165332, 165223,
+          226223, 236639, 211461, 190749],
    desc: "fixes the current preferences panel, some strict JavaScript " +
     "warnings, and unknown commands now work properly in 'autoperform'. It " +
-    "also replaces the four charset commands with two for simplicity."},
+    "also replaces the four charset commands with two for simplicity, shows " +
+    "more descriptive errors when /join fails, uses locale-correct dates in " +
+    "messages and logging. Also, it will connect to servers when run inside " +
+    "a tab, and now correctly handles non-ASCII case-mapping servers."},
   {id: "0.9.60dp", date: "03/02/2004", condition: Y,
    bugs: [],
    desc: "is a special version containing both the <b>0.9.60p</b> and " +
