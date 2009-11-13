070b100ed03c7cf18954b6f71a15cf80849d58fe
silver%warwickcompsoc.co.uk <silver%warwickcompsoc.co.uk>
2009-11-13 03:41:04 +0000

    Update links to follow weird AMO rules for src.

--- common/script/download.js
+++ common/script/download.js
@@ -9,7 +9,7 @@ function fixupDownloadBox() {
 	var rev = getLatestVersion();
 	
 	// Supported host.
-	document.write("<P><A HREF=\"https://addons.mozilla.org/addon/16?src=cz-homepage-js\">Install ChatZilla " + rev.id + " from Add-ons</A></P>\n");
+	document.write("<P><A HREF=\"https://addons.mozilla.org/addon/16?src=external-cz-homepage-js\">Install ChatZilla " + rev.id + " from Add-ons</A></P>\n");
 	document.write("<P CLASS=\"footnote\">(It's only " + rev.size + "KB.)</P>\n");
 	document.write("<P>ChatZilla works on <STRONG>" + host.name + " " + host.ver + "</STRONG>.</P>\n");
 	
